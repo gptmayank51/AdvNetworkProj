dee6454a50e8bc5a4bb84c22cb24c4a03aceba20
Mayank Gupta <gptmayank51@gmail.com>
2015-08-20 16:48:29 +0530

    Trying uncontrolled acknowledgments!

--- AdvNetworkProj/receiver.cpp
+++ AdvNetworkProj/receiver.cpp
@@ -67,10 +67,13 @@ main(int argc, char **argv)
 				}
 				else
 						printf("uh oh - something went wrong!\n");
-				sprintf_s(buf, "ack %d", msgcnt++);
-				printf("sending response \"%s\"\n", buf);
-				if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, addrlen) < 0)
+				for (int i = 0; i < 100; i++)
+				{
+					sprintf_s(buf, "ack %d", msgcnt++);
+					printf("sending response \"%s\"\n", buf);
+					if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, addrlen) < 0)
 						perror("sendto");
+				}
 		}
 		/* never exits */
 }
